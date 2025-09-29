c00a3b29ac37aab41c6c9f0119f53a0446b86a54
aafuchs-asf <anafuchs.asf@gmail.com>
2025-09-29 00:10:26 -0400

    Atividade 2

---
+++ Atividade2.py
@@ -0,0 +1,14 @@
+num = input('Digite um número inteiro: ')
+
+try:
+    numInt = int(num)
+
+    if int(num) == 0:
+        print('Atenção! O número não pode ser 0.')
+
+    else:    
+        div = 10/numInt
+        print(f'O resultado de 10 dividido por {num} é: {div}')
+
+except ValueError:
+    print(f'{num} não é um número inteiro!')
